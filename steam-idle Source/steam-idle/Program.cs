de36bde5f3fa0dd978cd4438b5ca254f02515c6b
Jonas Nilson <5990550+JonasNilson@users.noreply.github.com>
2023-09-17 10:47:26 +0200

    Rebuilt steam-idle.exe - HTTPS - Steamworks.NET 20.2.0 - Form renamed

--- steam-idle Source/steam-idle/Program.cs
+++ steam-idle Source/steam-idle/Program.cs
@@ -19,7 +19,7 @@ namespace steam_idle
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmMain(appId));
+            Application.Run(new FormSteamIdle(appId));
         }
     }
 }
