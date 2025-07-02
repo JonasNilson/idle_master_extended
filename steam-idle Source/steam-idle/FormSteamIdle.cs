195300b2e2bf50fcfbad5133df07322396d9d32c
Shioriko <72106761+shiokon@users.noreply.github.com>
2025-07-02 20:45:41 +0800

    Fall to no image when loading new games with no standard banner links

--- steam-idle Source/steam-idle/FormSteamIdle.cs
+++ steam-idle Source/steam-idle/FormSteamIdle.cs
@@ -8,7 +8,16 @@ namespace steam_idle
         public FormSteamIdle(long appid)
         {
             InitializeComponent();
-            picApp.Load($"https://cdn.akamai.steamstatic.com/steam/apps/{appid}/header_292x136.jpg");
+            try
+            {
+                picApp.Load($"https://cdn.akamai.steamstatic.com/steam/apps/{appid}/header_292x136.jpg");
+                picApp.Visible = true;
+            }
+            catch
+            {
+                picApp.Image = null;
+                picApp.Visible = false;
+            }
         }
 
         private void FormSteamIdle_Load(object sender, EventArgs e)
